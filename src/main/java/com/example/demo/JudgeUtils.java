7ecdeeba42acd037f0cf47143ffccbb32df3212c
shogokanzaki <b1018200@fun.ac.jp>
2022-01-20 15:40:44 +0900

    a

--- src/main/java/com/example/demo/JudgeUtils.java
+++ src/main/java/com/example/demo/JudgeUtils.java
@@ -6,7 +6,7 @@ import java.util.Date;
 public class JudgeUtils extends MiraiPrjApplication {
 
 	// 定数
-	public static final int AGE = 18;
+	public static final int AGE = 17;
 
 	/**
 	 * 課金対象か判定するメソッド<br>
@@ -26,7 +26,7 @@ public class JudgeUtils extends MiraiPrjApplication {
 		// 現在の年月日のみをセット
 		compareCal.set(curYear, curMonth, curDate);
 		// 年からプレイ可能年齢を引く
-		compareCal.add(Calendar.YEAR, -AGE);
+		compareCal.add(Calendar.YEAR, -AGE+1);
 
 		// 生年月日を設定
 		Calendar birthCal = Calendar.getInstance();
--- src/test/java/com/example/demo/JudgeUtilsTest.java
+++ src/test/java/com/example/demo/JudgeUtilsTest.java
@@ -126,7 +126,7 @@ class JudgeUtilsTest {
 	// 現在時刻：2018/04/01
 	// 合格条件：true
 	@Test
-	public void testIsRegisterdAge_18歳_プレイ可能() {
+	public void testIsRegisterdAge_17歳_プレイ可能() {
 
 		// 事前処理(Mock)
 		Calendar cal = Calendar.getInstance();
@@ -159,7 +159,7 @@ class JudgeUtilsTest {
 	// 現在時刻：2018/04/02
 	// 合格条件：true
 	@Test
-	public void testIsRegisterdAge_18歳_0402生まれ_プレイ可能() {
+	public void testIsRegisterdAge_17歳_0402生まれ_プレイ可能() {
 
 		// 事前処理(Mock)
 		Calendar cal = Calendar.getInstance();
@@ -192,7 +192,7 @@ class JudgeUtilsTest {
 	// 現在時刻：2017/04/01
 	// 合格条件：false
 	@Test
-	public void testIsRegisterdAge_17歳_プレイ不可() {
+	public void testIsRegisterdAge_16歳_プレイ不可() {
 		// 事前処理(Mock)
 				Calendar cal = Calendar.getInstance();
 				cal.clear();
